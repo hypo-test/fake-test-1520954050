8a76134df7b9fd494be712aa26d5cff189e76d5e
Hype <ivan.antsipau2@gmail.com>
2017-07-31 01:26:16 +0300

    bootstrap

---
+++ GroupMembersResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace fb_groups_intersector
+{
+    public class GroupMembersResponse
+    {
+        public GroupMember[] data { get; set; }
+        public Paging paging { get; set; }
+    }
+
+    public class Paging
+    {
+        public Cursors cursors { get; set; }
+        public string next { get; set; }
+    }
+
+    public class Cursors
+    {
+        public string before { get; set; }
+        public string after { get; set; }
+    }
+
+    public class GroupMember
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public bool administrator { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace fb_groups_intersector
 {
@@ -7,6 +9,21 @@ namespace fb_groups_intersector
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            using (var httpClient = new HttpClient())
+            {
+                var accessToken = "EAACEdEose0cBAIsVHL3XPwGk0yOfeGsbKeIq2nD70L1DlS3wwKcFdKndSRsC9r7EROBS48K2YlLE1SJ7Gmkti9X89Ry3vPqxygKr7iugjH8eZBKwxq611V9YdmLVoQM2NNWNrtLWjraJBotSJ5uZCa5hZBLs1V73f9JpOlsEwS9sIKzYJ5zEzRD4JbzUZBsZD";
+
+                var sourceGroupId = "246401099154708";
+                var sourceMembersResp = httpClient
+                    .GetStringAsync(
+                        $"https://graph.facebook.com/v2.10/{sourceGroupId}/members?access_token={accessToken}");
+
+                var souurceMembersResponse = JsonConvert.DeserializeObject<GroupMembersResponse>(sourceMembersResp.Result);
+                Console.WriteLine(JsonConvert.SerializeObject(souurceMembersResponse, Formatting.Indented));
+            }
+
+            Console.ReadLine();
         }
     }
 }
