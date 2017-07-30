553dfc9c00d001a34b663bc23f9e2ee9d9ccf4a4
Hype <ivan.antsipau2@gmail.com>
2017-07-31 00:58:33 +0300

    init proj by dotnert cli

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fb_groups_intersector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
